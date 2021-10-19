159c305f82723b0a3c447e3af7b3f06f60982ee8
MOHAMMAD FAROOQ <66942957+FAROOQkhan10@users.noreply.github.com>
2021-10-19 22:23:51 +0530

    Initial commit

---
+++ app/src/main/java/com/farooqkhan/learnersland/Fragments/ftab1.java
@@ -0,0 +1,117 @@
+package com.farooqkhan.learnersland.Fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.farooqkhan.learnersland.Adapter.categoryAdapter;
+import com.farooqkhan.learnersland.Book;
+import com.farooqkhan.learnersland.Model.categoryModel;
+import com.farooqkhan.learnersland.R;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ftab1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ftab1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ftab1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ftab1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ftab1 newInstance(String param1, String param2) {
+        ftab1 fragment = new ftab1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    Button btn;
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+    RecyclerView recyclerView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final ArrayList<categoryModel> categories = new ArrayList<>();
+        final categoryAdapter adapter = new categoryAdapter(getContext(), categories);
+
+          View view =  inflater.inflate(R.layout.fragment_ftab1, container, false);
+            recyclerView = view.findViewById(R.id.categorylist);
+            btn = view.findViewById(R.id.book_btn);
+                database.collection("category")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        categories.clear();
+                        for(DocumentSnapshot snapshot:value.getDocuments()){
+                            categoryModel model = snapshot.toObject(categoryModel.class);
+                            model.setCategoryId(snapshot.getId());
+                            categories.add(model);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+        recyclerView.setAdapter(adapter);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Book.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
+
+    }
+}
