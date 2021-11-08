f2e8f3e832d1c8afa37ef29ccfd0261d057bff0d
Mohammad Farooq <farooq208019@gmail.com>
2021-11-08 19:14:17 +0530

    Initial commit

--- app/src/main/java/com/farooqkhan/learnersland/MainActivity.java
+++ app/src/main/java/com/farooqkhan/learnersland/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-
         setContentView(R.layout.activity_main);
 
         tabLayout =  findViewById(R.id.tabLayout);
--- app/src/main/java/com/farooqkhan/learnersland/Model/BookModel.java
+++ app/src/main/java/com/farooqkhan/learnersland/Model/BookModel.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.farooqkhan.learnersland.Model;
 
 public class BookModel {
@@ -38,44 +37,3 @@ public class BookModel {
         this.url = url;
     }
 }
-=======
-package com.farooqkhan.learnersland.Model;
-
-public class BookModel {
-
-    String img,bookName,url;
-
-    public BookModel() {
-    }
-
-    public BookModel(String img, String bookName, String url) {
-        this.img = img;
-        this.bookName = bookName;
-        this.url = url;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-}
->>>>>>> github/master
